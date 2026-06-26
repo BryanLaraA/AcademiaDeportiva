4884ef23e3efd643a4c2551e1a97acdc78cebf00
isgutierrez11 <j11056641@gmail.com>
2026-06-26 15:12:31 -0600

    Se agrega la clase completa de disciplina
    se le agregan sus metodos de acceso y metodo constructor

--- src/disciplinas/Disciplina.java
+++ src/disciplinas/Disciplina.java
@@ -4,6 +4,9 @@
  */
 package disciplinas;
 
+import java.time.LocalDate;
+import participantes.Participantes;
+
 
 
 /**
@@ -11,5 +14,40 @@ package disciplinas;
  * @author Student
  */
 public class Disciplina extends Participantes {
+    
+    private Categoria categoria;
+    private int capMax; 
+
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public int getCapMax() {
+        return capMax;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public void setCapMax(int capMax) {
+        this.capMax = capMax;
+    }
+
+    
+    
+    public Disciplina(int identificacion, String nombrecompleto, LocalDate fechadenacimiento, int telefono) {
+        super(identificacion, nombrecompleto, fechadenacimiento, telefono);
+    }
+
+    @Override
+    public String toString() {
+        return "Disciplina{" + "categoria=" + categoria + ", capMax=" + capMax + '}';
+    }
+    
+   
+
+   
     
 }
