515f5e8997fd03fbdbed8dc1c66e85fb7406f3de
Bryan <bryanlaraarguello@gmail.com>
2026-06-26 15:06:04 -0600

    Cambios en participantes creacion del DEV remoto

--- src/participantes/Participantes.java
+++ src/participantes/Participantes.java
@@ -5,34 +5,12 @@ import disciplinas.Disciplina;
 
 public class Participantes extends Persona {
     
-    private int identificacion;
-    private String nombrecompleto;
-    private LocalDate fechadenacimiento;
-    private int telefono;
+    
     private Disciplina diciplina;
 
-    public int getIdentificacion() {
-        return identificacion;
-    }
-
-    public String getNombrecompleto() {
-        return nombrecompleto;
-    }
-
-    public LocalDate getFechadenacimiento() {
-        return fechadenacimiento;
-    }
-
-    public int getTelefono() {
-        return telefono;
-    }
-
+    
     public Disciplina getDiciplina() {
         return diciplina;
-    }
-
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
     }
 
     public void setDiciplina(Disciplina diciplina) {
